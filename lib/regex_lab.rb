b9e66db604093a59dbd97195f95cefb17795b203
Natalia Castro <nat.alvz@gmail.com>
2018-05-13 20:01:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,6 @@
 def starts_with_a_vowel?(word)
-if word = word.scan(/^[aeiou]$)
-  return true
-else
-  false
-end
+match.each do |word|
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
