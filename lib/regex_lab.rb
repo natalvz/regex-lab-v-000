3082806450d7bd7b6951904ea8062ac55ef6184a
Natalia Castro <nat.alvz@gmail.com>
2018-05-13 20:04:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
 if word = word.scan(/^[aeiouAEIOU]$/)
   return true
-else word = word.scan(/^[aeiouAEIOU]$/)
+else word = word.scan(/^[^aeiouAEIOU]$/)
   false
 end
 end
